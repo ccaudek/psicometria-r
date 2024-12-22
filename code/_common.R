e3f0e206ce514681009ea2149237a59ac16fdc3c
Corrado Caudek <corrado.caudek@unifi.it>
2024-12-22 11:06:49 +0100

    updeted probability section

--- code/_common.R
+++ code/_common.R
@@ -14,23 +14,33 @@ pacman::p_load(
   bayesplot, # Grafici diagnostici e di visualizzazione per modelli bayesiani
   patchwork, # Composizione di grafici ggplot
   gridExtra, # Funzioni per visualizzare layout a griglia
-  ggExtra, # Funzionalità extra per ggplot2 (grafici marginali)
-  ggpubr, # Funzionalità di pubblicazione per grafici ggplot
-  viridis, # Palette di colori per ggplot (ideale per scale percettivamente uniformi)
-  MASS # Pacchetto per metodi statistici aggiuntivi
+  see, # Provides palette_okabeito
+  tidyr # Funzionalità per creare tidy data
 )
 
-# Knitr Chunk Options ----------------------------------------------------------
+options(tidyverse.quiet = TRUE)
 
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  # cache = TRUE,
-  fig.retina = 2,
+  width = 72,
+  tidy.opts = list(width.cutoff = 72, tidy = TRUE),
+  out.width = "70%",
+  fig.align = "center",
   fig.width = 6,
-  fig.asp = 2/3,
-  fig.show = "hold"
+  fig.height = 3.708, # width * 1 / phi (phi = golden ratio)
+  fig.show = "hold",
+  R.options = list(
+    digits = 3,
+    width = 76
+  ),
+  formatR.indent = 2,
+  dplyr.summarise.inform = FALSE,
+  dplyr.print_min = 5,
+  dplyr.print_max = 5,
+  ggrepel.max.overlaps = 100
 )
+
 
 options(
   dplyr.print_min = 6,
@@ -38,25 +48,22 @@ options(
   pillar.max_footer_lines = 2,
   pillar.min_chars = 15,
   stringr.view_n = 6,
-  # Temporarily deactivate cli output for quarto
+  # Disattiva temporaneamente l'output CLI per Quarto
   cli.num_colors = 0,
   cli.hyperlink = FALSE,
   pillar.bold = TRUE,
-  width = 77 # 80 - 3 for #> comment
+  width = 77 # 80 - 3 per il commento #>
 )
 
-# ggplot2::theme_set(ggplot2::theme_gray(12))
-color_scheme_set("brightblue") # bayesplot
 
 # Theme Settings ---------------------------------------------------------------
 
+# ggplot2::theme_set(ggplot2::theme_gray(12))
 theme_set(bayesplot::theme_default(base_size = 13, base_family = "sans"))
+
+color_scheme_set("brightblue") # bayesplot
 
 # Seed -------------------------------------------------------------------------
 
 set.seed(42) # Fissa il seme per la riproducibilità
-
-options(digits = 3)
-options(tidyverse.quiet = TRUE)
-
 
